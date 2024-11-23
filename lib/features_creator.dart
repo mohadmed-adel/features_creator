d5049caa3ef8323dd9d1082b874c736eac575d16
Mohamed Adel <mohadel1033@gmail.com>
2024-11-23 16:14:14 +0200

    create helpers

--- lib/features_creator.dart
+++ lib/features_creator.dart
@@ -4,8 +4,6 @@ import 'dart:developer';
 import 'dart:io';
 
 void main({String? featureName, bool createCore = false}) {
-  print("createCore: $createCore");
-
   if (featureName == null || featureName.isEmpty) {
     log('Error: featureName is required.');
     return;
@@ -61,49 +59,191 @@ Future<void> configureDependencies() async {
   getIt.init();
 }
 ''',
+    'lib/core/router/router.dart': '''
+part 'router.gr.dart';
+
+@singleton
+@AutoRouterConfig(replaceInRouteName: 'Screen,Route')
+class AppRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+       // AutoRoute(page: SplashRoute.page, initial: true), 
+      ];
+}
+''',
+    'lib/core/network/dio_helper.dart': '''
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:network_logger/network_logger.dart';
+import 'package:tellme/core/base-models/base-success-response-model/base_success_response_model.dart';
+import 'package:tellme/core/network/network_helper.dart';
+import 'package:tellme/core/network/token_handler.dart';
+
+import '../../core/network/api_strings.dart';
+import '../../injection.dart';
+import '../storage/storage_keys.dart';
+import '../storage/storage_services.dart';
+import 'network_interface.dart';
+
+@LazySingleton(as: BaseNetwork)
+class DioHelper implements BaseNetwork {
+  final Dio dio = Dio();
+
+  DioHelper() {
+    dio.options.baseUrl = ApiStrings.apiUrl;
+    dio.options.receiveDataWhenStatusError = true;
+    dio.options.connectTimeout = const Duration(seconds: 15);
+    dio.options.receiveTimeout = const Duration(seconds: 15);
+
+    dio.interceptors.add(DioNetworkLogger());
+  }
+
+  _buildHeader() async {
+    String? token = await TokenHandler.getUserToken();
+    String langCode = getIt
+            .get<StorageService>()
+            .valueGetter(key: StorageKeys.activeLocale) ??
+        'en';
+    dio.options.headers = {
+      "App-id": ApiStrings.appId,
+      if (token != null) "Authorization": "Bearer " +token,
+      "LanguageCode": langCode,
+    };
+  }
+
+  @override
+  Future<BaseSuccessResponseModel> get(String endPoint,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      await _buildHeader();
+      final response = await dio.get(ApiStrings.apiUrl + endPoint,
+          queryParameters: queryParameters);
+
+      return BaseSuccessResponseModel.fromJson(response.data);
+    } on DioException catch (error) {
+      throw await NetworkUtils.networkExceptions(error);
+    }
+  }
+
+  @override
+  Future<BaseSuccessResponseModel> delete(String endPoint, int id) async {
+    try {
+      await _buildHeader();
+      final response = await dio.delete( endPoint+/+id);
+
+      return BaseSuccessResponseModel.fromJson(response.data);
+    } on DioException catch (error) {
+      throw await NetworkUtils.networkExceptions(error);
+    }
+  }
+
+  @override
+  Future<BaseSuccessResponseModel> post(String endPoint,
+      {Map<String, dynamic>? data, ProgressCallback? progressCallback}) async {
+    try {
+      await _buildHeader();
+      final response = await dio.post(endPoint,
+          data: data, onReceiveProgress: progressCallback);
+
+      return BaseSuccessResponseModel.fromJson(response.data);
+    } on DioException catch (error) {
+      throw await NetworkUtils.networkExceptions(error);
+    }
+  }
+
+  @override
+  Future<BaseSuccessResponseModel> update(String endPoint,
+      {Map<String, dynamic>? data}) async {
+    try {
+      await _buildHeader();
+      final response = await dio.patch(endPoint, data: data);
+
+      return BaseSuccessResponseModel.fromJson(response.data);
+    } on DioException catch (error) {
+      throw await NetworkUtils.networkExceptions(error);
+    }
+  }
+
+  @override
+  Future<BaseSuccessResponseModel> uploadImages(
+      String endPoint, Map<String, File> data) async {
+    try {
+      await _buildHeader();
+      FormData formData = FormData.fromMap(data);
+      formData.files.add(MapEntry(data.entries.first.key,
+          await MultipartFile.fromFile(data.entries.first.value.path)));
+      final response = await dio.post(endPoint, data: formData);
+
+      return BaseSuccessResponseModel.fromJson(response.data);
+    } on DioException catch (error) {
+      throw await NetworkUtils.networkExceptions(error);
+    }
+  }
+}
+''',
+    'lib/core/network/network_interface.dart': '''
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:tellme/core/base-models/base-success-response-model/base_success_response_model.dart';
+
+abstract class BaseNetwork {
+  Future<BaseSuccessResponseModel> get(String endPoint,
+      {Map<String, dynamic>? queryParameters});
+  Future<BaseSuccessResponseModel> post(String endPoint,
+      {Map<String, dynamic>? data, ProgressCallback? progressCallback});
+  Future<BaseSuccessResponseModel> delete(String endPoint, int id);
+  Future<BaseSuccessResponseModel> update(String endPoint,
+      {Map<String, dynamic>? data});
+  Future<BaseSuccessResponseModel> uploadImages(
+      String endPoint, Map<String, File> data);
+}
+''',
+    'lib/core/network/token_handler.dart': '''
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+
+import '../storage/secure_storage.dart';
+import '../storage/storage_keys.dart';
+
+class TokenHandler {
+  TokenHandler._();
+
+  static Future<String?> getUserToken() async {
+    try {
+      final token =
+          await SecureStorage.getInstance().getValue(StorageKeys.accessToken);
+      return token;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static clearToken() async {
+    await SecureStorage.getInstance().deleteValue(StorageKeys.accessToken);
+  }
+}
+''',
+    'lib/core/network/api_strings.dart': '''
+class ApiStrings {
+  static const String apiUrl = '';
+  static const String appId = 'your_app_id_here';
+}
+''',
   };
 
   // Files to generate dynamically based on feature name
   final featureFiles = {
-    // Datasources
     'lib/features/$featureName/data/datasources/base_${featureName}_datasource.dart':
         '''
 abstract class Base${capitalize(featureName)}DataSource {
   // Define the abstract methods here for your data source.
 }
 ''',
-    // Implementation of the abstract class
-    'lib/features/$featureName/data/datasources/${featureName}_datasource_impl.dart':
-        '''
-import 'base_${featureName}_datasource.dart';
-import 'package:injectable/injectable.dart';
-
-@Injectable(as: Base${capitalize(featureName)}DataSource)
-class ${capitalize(featureName)}DataSourceImpl implements Base${capitalize(featureName)}DataSource {
-  // Implement abstract methods here
-}
-''',
-    // Repositories
-    'lib/features/$featureName/domain/repositories/base_${featureName}_repository.dart':
-        '''
-abstract class Base${capitalize(featureName)}Repository {
-  // Define the abstract methods here for your repository.
-}
-''',
-    // Implementation of the abstract class
-    'lib/features/$featureName/data/repositories/${featureName}_repository_impl.dart':
-        '''
-import '../../domain/repositories/base_${featureName}_repository.dart';
-import 'package:injectable/injectable.dart';
-
-@Injectable(as: Base${capitalize(featureName)}Repository)
-class ${capitalize(featureName)}RepositoryImpl implements Base${capitalize(featureName)}Repository {
-  // Implement abstract methods here
-}
-''',
   };
 
-  // Generate directories and files based on the flag
   final directories = [
     if (createCore) ...coreDirectories,
     ...featureDirectories,
@@ -111,16 +251,14 @@ class ${capitalize(featureName)}RepositoryImpl implements Base${capitalize(featu
 
   final files = {
     if (createCore) ...coreFiles,
-    ...featureFiles, // Adds the dynamic feature-specific files
+    ...featureFiles,
   };
 
-  // Creating folders
   for (var dir in directories) {
     Directory(dir).createSync(recursive: true);
     log('Folder $dir created successfully ✅');
   }
 
-  // Creating files
   files.forEach((filePath, content) {
     File(filePath).createSync(recursive: true);
     File(filePath).writeAsStringSync(content);
@@ -128,7 +266,6 @@ class ${capitalize(featureName)}RepositoryImpl implements Base${capitalize(featu
   });
 }
 
-// Helper function to capitalize the feature name for class name generation
 String capitalize(String text) {
   if (text.isEmpty) return text;
   return text[0].toUpperCase() + text.substring(1);
